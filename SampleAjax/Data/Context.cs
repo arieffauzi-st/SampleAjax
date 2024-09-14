1db4834617be8ea92c8b7e97fc4935aa1ede62df
arieffauzi-st <arieffauzi_st@gmail.com>
2024-09-15 03:20:31 +0700

    first commit

---
+++ SampleAjax/Data/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SampleAjax.Data
+{
+    public class Context:DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ SampleAjax/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SampleAjax.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
